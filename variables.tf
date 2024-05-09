3cd8e920c7d9e91be8a269be4a90994c650ff1f3
Victor Galvez <vhgalvez@gmail.com>
2024-05-10 00:07:16 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-# variables.tf
 variable "flatcar_base_image" {
   description = "Path to the base VM image for Flatcar Container Linux"
   type        = string
@@ -20,24 +19,25 @@ variable "cluster_domain" {
 }
 
 variable "vm_definitions" {
-  description = "Definitions of virtual machines including CPU, memory configuration, and OS type"
+  description = "Definitions of virtual machines including CPU, memory configuration, and IP for Flatcar"
   type = map(object({
     cpus   = number
     memory = number
+    ip     = string
+    type   = string
+  }))
+}
+
+variable "vm_rockylinux_definitions" {
+  description = "Definitions of virtual machines including CPU, memory configuration, and IP for Rocky Linux"
+  type = map(object({
+    cpus   = number
+    memory = number
+    ip     = string
   }))
 }
 
 variable "ssh_keys" {
   description = "List of SSH keys to inject into VMs"
   type        = list(string)
-}
-
-
-variable "vm_rockylinux_definitions" {
-  description = "Definitions of Rocky Linux virtual machines including CPU, memory configuration, and IP"
-  type = map(object({
-    cpus   = number
-    memory = number
-    ip     = string
-  }))
 }
